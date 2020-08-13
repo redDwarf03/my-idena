e13c60469fc3fe76c729b9cf0bdc1cf1dcdf7eb1
redDwarf03 <a_sacris_sylvain@yahoo.fr>
2020-08-13 22:57:39 +0200

    long session

--- lib/beans/validation,_session_infos.dart
+++ lib/beans/validation,_session_infos.dart
@@ -7,10 +7,12 @@ import 'package:flutter/services.dart';
 import 'package:my_idena/beans/rpc/flip_get_request.dart';
 import 'package:my_idena/beans/rpc/flip_get_response.dart';
 import 'package:my_idena/beans/rpc/flip_longHashes_request.dart';
+import 'package:my_idena/beans/rpc/flip_longHashes_response.dart';
 import 'package:my_idena/beans/rpc/flip_shortHashes_request.dart';
 import 'package:my_idena/beans/rpc/flip_shortHashes_response.dart';
 import 'package:my_idena/beans/rpc/flip_words_request.dart';
 import 'package:my_idena/beans/rpc/flip_words_response.dart';
+import 'package:my_idena/beans/test/flip_examples.dart';
 import 'package:my_idena/utils/epoch_period.dart' as EpochPeriod;
 import 'package:my_idena/utils/sharedPreferencesHelper.dart';
 import 'package:ethereum_util/ethereum_util.dart';
@@ -67,6 +69,7 @@ Future<ValidationSessionInfo> getValidationSessionInfo(String typeSession,
   String method;
   simulationMode = await getSimulationMode();
 
+  Map<String, dynamic> mapExemple;
   switch (typeSession) {
     case EpochPeriod.ShortSession:
       {
@@ -84,127 +87,23 @@ Future<ValidationSessionInfo> getValidationSessionInfo(String typeSession,
 
   FlipShortHashesRequest flipShortHashesRequest;
   FlipShortHashesResponse flipShortHashesResponse;
+  FlipLongHashesRequest flipLongHashesRequest;
+  FlipLongHashesResponse flipLongHashesResponse;
 
   try {
     HttpClient httpClient = new HttpClient();
     IdenaSharedPreferences idenaSharedPreferences =
         await SharedPreferencesHelper.getIdenaSharedPreferences();
 
-    // Short or long Hashes
     if (simulationMode) {
-      Map<String, dynamic> mapExemple = {
-        "jsonrpc": "2.0",
-        "id": 19,
-        "result": [
-          {
-            "hash":
-                "bafkreial55rw3dirdlrivcjsnnxaswfrloxuk4pbfssxbwhheqbo44crra",
-            "ready": true,
-            "extra": false,
-            "available": true
-          },
-          {
-            "hash":
-                "bafkreiasdvce4g2wlmkia5bmj27snlsa44imfeu2e5whg3r6rea57avmwa",
-            "ready": true,
-            "extra": false,
-            "available": true
-          },
-          {
-            "hash":
-                "bafkreiaxmbxoy3rystvl4hcfd46uvbxxqy4op7ivvixhqtf5fvk4vjijpq",
-            "ready": true,
-            "extra": false,
-            "available": true
-          },
-          {
-            "hash":
-                "bafkreidipdlvqzvguqpimwi5g72bu4kknrxczitsu2mrrod3ctazfynqem",
-            "ready": true,
-            "extra": false,
-            "available": true
-          },
-          {
-            "hash":
-                "bafkreiel3bwd35dq64zflh2oxxzh256kl57n2d4n5ezkl3eso7quollm5m",
-            "ready": true,
-            "extra": false,
-            "available": true
-          },
-          {
-            "hash":
-                "bafkreiewtcyikpwfrbrbnbmseehqpy3rxozo7fgyihjmck6ltjcxx6yn4q",
-            "ready": true,
-            "extra": false,
-            "available": true
-          },
-          {
-            "hash":
-                "bafkreihs62lgfparrtutsrzup55vpgxx7o7nocakwmcxhojf3fzuen5vqy",
-            "ready": true,
-            "extra": true,
-            "available": true
-          },
-        ]
-      };
-
-      /*Map<String, dynamic> mapExemple = {
-        "jsonrpc": "2.0",
-        "id": 19,
-        "result": [
-          {
-            "hash":
-                "bafkreia4khjzwy5kt4k25djgvabdnzz5vi5gvwidk7e5kcvdjdr4x3lygm",
-            "ready": true,
-            "extra": false,
-            "available": true
-          },
-          {
-            "hash":
-                "2",
-            "ready": true,
-            "extra": false,
-            "available": true
-          },
-          {
-            "hash":
-                "3",
-            "ready": true,
-            "extra": false,
-            "available": true
-          },
-          {
-            "hash":
-                "4",
-            "ready": true,
-            "extra": false,
-            "available": true
-          },
-          {
-            "hash":
-                "5",
-            "ready": true,
-            "extra": false,
-            "available": true
-          },
-          {
-            "hash":
-                "6",
-            "ready": true,
-            "extra": false,
-            "available": true
-          },
-          {
-            "hash":
-                "7",
-            "ready": true,
-            "extra": true,
-            "available": true
-          },
-        ]
-      };*/
-
-      flipShortHashesResponse = FlipShortHashesResponse.fromJson(mapExemple);
+      if (typeSession == EpochPeriod.ShortSession) {
+        flipShortHashesResponse = FlipShortHashesResponse.fromJson(
+            FlipExamples().getMapExample(typeSession));
+      }
+      if (typeSession == EpochPeriod.LongSession) {
+        flipLongHashesResponse = FlipLongHashesResponse.fromJson(
+            FlipExamples().getMapExample(typeSession));
+      }
     } else {
       HttpClientRequest request =
           await httpClient.postUrl(Uri.parse(idenaSharedPreferences.apiUrl));
@@ -216,37 +115,69 @@ Future<ValidationSessionInfo> getValidationSessionInfo(String typeSession,
         'id': 101,
         'key': idenaSharedPreferences.keyApp
       };
-      flipShortHashesRequest = FlipShortHashesRequest.fromJson(map);
-      request.add(utf8.encode(json.encode(flipShortHashesRequest.toJson())));
-      HttpClientResponse response = await request.close();
-      if (response.statusCode == 200) {
-        String reply = await response.transform(utf8.decoder).join();
 
-        flipShortHashesResponse = flipShortHashesResponseFromJson(reply);
+      if (typeSession == EpochPeriod.ShortSession) {
+        flipShortHashesRequest = FlipShortHashesRequest.fromJson(map);
+        request.add(utf8.encode(json.encode(flipShortHashesRequest.toJson())));
+        HttpClientResponse response = await request.close();
+        if (response.statusCode == 200) {
+          String reply = await response.transform(utf8.decoder).join();
+
+          flipShortHashesResponse = flipShortHashesResponseFromJson(reply);
+        }
+      }
+      if (typeSession == EpochPeriod.LongSession) {
+        flipLongHashesRequest = FlipLongHashesRequest.fromJson(map);
+        request.add(utf8.encode(json.encode(flipLongHashesRequest.toJson())));
+        HttpClientResponse response = await request.close();
+        if (response.statusCode == 200) {
+          String reply = await response.transform(utf8.decoder).join();
+
+          flipLongHashesResponse = flipLongHashesResponseFromJson(reply);
+        }
       }
     }
 
     FlipGetResponse flipGetResponse;
     FlipWordsResponse flipWordsResponse;
-    List<ValidationSessionInfoFlips> listSessionValidationFlip =
-        new List(flipShortHashesResponse.result.length);
+    List<ValidationSessionInfoFlips> listSessionValidationFlip;
+    if (typeSession == EpochPeriod.ShortSession) {
+      listSessionValidationFlip =
+          new List(flipShortHashesResponse.result.length);
+    }
+    if (typeSession == EpochPeriod.LongSession) {
+      listSessionValidationFlip =
+          new List(flipLongHashesResponse.result.length);
+    }
 
     // TODO : distinguer Short/long
-    for (int i = 0; i < flipShortHashesResponse.result.length; i++) {
+    for (int i = 0; i < listSessionValidationFlip.length; i++) {
       ValidationSessionInfoFlips validationSessionInfoFlips =
           new ValidationSessionInfoFlips();
-      validationSessionInfoFlips.hash = flipShortHashesResponse.result[i].hash;
-      validationSessionInfoFlips.ready =
-          flipShortHashesResponse.result[i].ready;
-      validationSessionInfoFlips.extra =
-          flipShortHashesResponse.result[i].extra;
-      validationSessionInfoFlips.available =
-          flipShortHashesResponse.result[i].available;
+      if (typeSession == EpochPeriod.ShortSession) {
+        validationSessionInfoFlips.hash =
+            flipShortHashesResponse.result[i].hash;
+        validationSessionInfoFlips.ready =
+            flipShortHashesResponse.result[i].ready;
+        validationSessionInfoFlips.extra =
+            flipShortHashesResponse.result[i].extra;
+        validationSessionInfoFlips.available =
+            flipShortHashesResponse.result[i].available;
+      }
+      if (typeSession == EpochPeriod.LongSession) {
+        validationSessionInfoFlips.hash = flipLongHashesResponse.result[i].hash;
+        validationSessionInfoFlips.ready =
+            flipLongHashesResponse.result[i].ready;
+        validationSessionInfoFlips.extra =
+            flipLongHashesResponse.result[i].extra;
+        validationSessionInfoFlips.available =
+            flipLongHashesResponse.result[i].available;
+      }
 
       // get Flip
       if (simulationMode) {
-        String data = await loadAssets(
-            flipShortHashesResponse.result[i].hash + "_images");
+        String data =
+            await loadAssets(validationSessionInfoFlips.hash + "_images");
         flipGetResponse = flipGetResponseFromJson(data);
       } else {
         HttpClientRequest requestFlip =
@@ -255,7 +186,7 @@ Future<ValidationSessionInfo> getValidationSessionInfo(String typeSession,
 
         Map<String, dynamic> mapFlip = {
           'method': FlipGetRequest.METHOD_NAME,
-          'params': [flipShortHashesResponse.result[i].hash],
+          'params': [validationSessionInfoFlips.hash],
           'id': 101,
           'key': idenaSharedPreferences.keyApp
         };
@@ -278,74 +209,94 @@ Future<ValidationSessionInfo> getValidationSessionInfo(String typeSession,
       Decoded privateImages;
       List listImages = new List(4);
       List orders = new List(2);
-    if (flipGetResponse.result.privateHex != null &&
-        flipGetResponse.result.privateHex != '0x') {
-      // ;[images] = decode(publicHex || hex)
-      if (flipGetResponse.result.publicHex != null) {
-        images = Rlp.decode(
-            Uint8List.fromList(toBuffer(flipGetResponse.result.publicHex)),
-            true);
-      } else {
-        if (flipGetResponse.result.hex != null) {
+      if (flipGetResponse.result.privateHex != null &&
+          flipGetResponse.result.privateHex != '0x') {
+        // ;[images] = decode(publicHex || hex)
+        if (flipGetResponse.result.publicHex != null) {
           images = Rlp.decode(
-              Uint8List.fromList(toBuffer(flipGetResponse.result.hex)), true);
+              Uint8List.fromList(toBuffer(flipGetResponse.result.publicHex)),
+              true);
+        } else {
+          if (flipGetResponse.result.hex != null) {
+            images = Rlp.decode(
+                Uint8List.fromList(toBuffer(flipGetResponse.result.hex)), true);
+          }
         }
+
+        // let privateImages
+        // ;[privateImages, orders] = decode(privateHex)
+        privateImages = Rlp.decode(
+            Uint8List.fromList(toBuffer(flipGetResponse.result.privateHex)),
+            true);
+
+        // images = images.concat(privateImages)
+        imageUint8_1 = images.data[0][0];
+        imageUint8_2 = images.data[0][1];
+        imageUint8_3 = privateImages.data[0][0];
+        imageUint8_4 = privateImages.data[0][1];
+        listImages[0] = imageUint8_1;
+        listImages[1] = imageUint8_2;
+        listImages[2] = imageUint8_3;
+        listImages[3] = imageUint8_4;
+        orders = privateImages.data[1];
+      } else {
+        // TODO: implement this case
+        // ;[images, orders] = decode(hex)
+        var images3;
+        images3 = Rlp.decode(
+            Uint8List.fromList(toBuffer(flipGetResponse.result.hex)), true);
       }
 
-      // let privateImages
-      // ;[privateImages, orders] = decode(privateHex)
-      privateImages = Rlp.decode(
-          Uint8List.fromList(toBuffer(flipGetResponse.result.privateHex)),
-          true);
-
-      // images = images.concat(privateImages)
-      imageUint8_1 = images.data[0][0];
-      imageUint8_2 = images.data[0][1];
-      imageUint8_3 = privateImages.data[0][0];
-      imageUint8_4 = privateImages.data[0][1];
-      listImages[0] = imageUint8_1;
-      listImages[1] = imageUint8_2;
-      listImages[2] = imageUint8_3;
-      listImages[3] = imageUint8_4;
-      orders = privateImages.data[1];
-    } else {
-      // TODO: implement this case
-      // ;[images, orders] = decode(hex)
-      var images3;
-      images3 = Rlp.decode(
-          Uint8List.fromList(toBuffer(flipGetResponse.result.hex)), true);
-    }
-
-      // hash,
-      // decoded: true,
-      // images: images.map(buffer =>
-      //  URL.createObjectURL(new Blob([buffer], {type: 'image/png'}))
-      //),
-      // orders: orders.map(order => order.map(([idx = 0]) => idx)),
-      // hex: '',
-
-      String order1 = orders[0][0].toString().replaceAll('[', '').replaceAll(']', '');
-      String order2 = orders[0][1].toString().replaceAll('[', '').replaceAll(']', '');
-      String order3 = orders[0][2].toString().replaceAll('[', '').replaceAll(']', '');
-      String order4 = orders[0][3].toString().replaceAll('[', '').replaceAll(']', '');
-      print(i.toString() + " - flip 1: " + order1 + ', ' + order2 + ', '+ order3 + ', ' + order4);
+      String order1 =
+          orders[0][0].toString().replaceAll('[', '').replaceAll(']', '');
+      String order2 =
+          orders[0][1].toString().replaceAll('[', '').replaceAll(']', '');
+      String order3 =
+          orders[0][2].toString().replaceAll('[', '').replaceAll(']', '');
+      String order4 =
+          orders[0][3].toString().replaceAll('[', '').replaceAll(']', '');
+      print(i.toString() +
+          " - flip 1: " +
+          order1 +
+          ', ' +
+          order2 +
+          ', ' +
+          order3 +
+          ', ' +
+          order4);
       validationSessionInfoFlips.listImagesLeft = new List<Uint8List>(4);
-      validationSessionInfoFlips.listImagesLeft[0] = listImages[int.tryParse(order1) ?? 0];
-      validationSessionInfoFlips.listImagesLeft[1] = listImages[int.tryParse(order2) ?? 0];
-      validationSessionInfoFlips.listImagesLeft[2] = listImages[int.tryParse(order3) ?? 0];
-      validationSessionInfoFlips.listImagesLeft[3] = listImages[int.tryParse(order4) ?? 0];
+      validationSessionInfoFlips.listImagesLeft[0] =
+          listImages[int.tryParse(order1) ?? 0];
+      validationSessionInfoFlips.listImagesLeft[1] =
+          listImages[int.tryParse(order2) ?? 0];
+      validationSessionInfoFlips.listImagesLeft[2] =
+          listImages[int.tryParse(order3) ?? 0];
+      validationSessionInfoFlips.listImagesLeft[3] =
+          listImages[int.tryParse(order4) ?? 0];
 
       // TODO .. dirty
       order1 = orders[1][0].toString().replaceAll('[', '').replaceAll(']', '');
       order2 = orders[1][1].toString().replaceAll('[', '').replaceAll(']', '');
       order3 = orders[1][2].toString().replaceAll('[', '').replaceAll(']', '');
       order4 = orders[1][3].toString().replaceAll('[', '').replaceAll(']', '');
-      print(i.toString() + " - flip 2: " + order1 + ', ' + order2 + ', '+ order3 + ', ' + order4);
+      print(i.toString() +
+          " - flip 2: " +
+          order1 +
+          ', ' +
+          order2 +
+          ', ' +
+          order3 +
+          ', ' +
+          order4);
       validationSessionInfoFlips.listImagesRight = new List<Uint8List>(4);
-      validationSessionInfoFlips.listImagesRight[0] = listImages[int.tryParse(order1) ?? 0];
-      validationSessionInfoFlips.listImagesRight[1] = listImages[int.tryParse(order2) ?? 0];
-      validationSessionInfoFlips.listImagesRight[2] = listImages[int.tryParse(order3) ?? 0];
-      validationSessionInfoFlips.listImagesRight[3] = listImages[int.tryParse(order4) ?? 0];
+      validationSessionInfoFlips.listImagesRight[0] =
+          listImages[int.tryParse(order1) ?? 0];
+      validationSessionInfoFlips.listImagesRight[1] =
+          listImages[int.tryParse(order2) ?? 0];
+      validationSessionInfoFlips.listImagesRight[2] =
+          listImages[int.tryParse(order3) ?? 0];
+      validationSessionInfoFlips.listImagesRight[3] =
+          listImages[int.tryParse(order4) ?? 0];
 
       // get Words
       /*if (simulationMode) {
