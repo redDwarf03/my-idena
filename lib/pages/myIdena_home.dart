b262b1940ff80ec11d2174de30e190fe704edc25
redDwarf03 <a_sacris_sylvain@yahoo.fr>
2020-08-16 23:21:37 +0200

    clean code

--- lib/pages/myIdena_home.dart
+++ lib/pages/myIdena_home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_idena/beans/rpc/dna_all.dart';
 import 'package:my_idena/myIdena_app/tabIcon_data.dart';
+import 'package:my_idena/pages/screens/parameters_screen.dart';
 import 'package:my_idena/pages/screens/validation_session_screen.dart';
 import 'views/bottom_bar_view.dart';
 import 'package:my_idena/main.dart';
@@ -9,13 +10,11 @@ import 'screens/home_screen.dart';
 import 'screens/about_screen.dart';
 
 class Home extends StatefulWidget {
-
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home>
-    with TickerProviderStateMixin {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
   AnimationController animationController;
 
   List<TabIconData> tabIconsList = TabIconData.tabIconsList;
@@ -100,8 +99,18 @@ class _HomeState extends State<Home>
                   return;
                 }
                 setState(() {
+                  tabBody = ValidationSessionScreen(
+                      animationController: animationController);
+                });
+              });
+            } else if (index == 2) {
+              animationController.reverse().then<dynamic>((data) {
+                if (!mounted) {
+                  return;
+                }
+                setState(() {
                   tabBody =
-                      ValidationSessionScreen(animationController: animationController);
+                      ParametersScreen(animationController: animationController);
                 });
               });
             } else if (index == 3) {
--- lib/pages/screens/about_screen.dart
+++ lib/pages/screens/about_screen.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:my_idena/myIdena_app/myIdena_app_theme.dart';
 import 'package:my_idena/pages/views/about_view.dart';
 import 'package:my_idena/pages/views/donation_view.dart';
-import 'package:my_idena/pages/views/paramRPC_view.dart';
-import 'package:my_idena/pages/views/portofolio_view.dart';
-import 'package:my_idena/pages/views/profile_view.dart';
 import 'package:my_idena/pages/views/title_view.dart';
 import 'package:my_idena/utils/app_localizations.dart';
 
