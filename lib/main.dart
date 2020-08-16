dc1e8169e69af54abdfd09e2765510c71eeb809b
redDwarf03 <a_sacris_sylvain@yahoo.fr>
2020-08-16 23:18:21 +0200

    rename dart file

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:my_idena/beans/rpc/dna_all.dart';
 import 'package:my_idena/myIdena_app/myIdena_app_theme.dart';
-import 'package:my_idena/pages/screens/on_boarding.dart';
+import 'package:my_idena/pages/screens/on_boarding_screen.dart';
 import 'package:my_idena/utils/app_localizations.dart';
 import 'package:my_idena/utils/epoch_period.dart' as EpochPeriod;
 
