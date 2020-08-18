c3d17969eb4f7c5d3af6ac74d15f99cc0a05fe11
redDwarf03 <a_sacris_sylvain@yahoo.fr>
2020-08-18 16:56:05 +0200

    List of recent transactions functionnality

---
+++ lib/pages/views/transactions_view.dart
@@ -0,0 +1,400 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:my_idena/beans/rpc/bcn_transactions_response.dart';
+import 'package:my_idena/beans/rpc/dna_all.dart';
+import 'package:my_idena/beans/rpc/httpService.dart';
+import 'package:my_idena/main.dart';
+
+import 'package:my_idena/myIdena_app/myIdena_app_theme.dart';
+import 'package:my_idena/utils/app_localizations.dart';
+
+HttpService httpService = HttpService();
+
+class TransactionsView extends StatefulWidget {
+  final AnimationController animationController;
+  final Animation animation;
+
+  const TransactionsView({Key key, this.animationController, this.animation})
+      : super(key: key);
+
+  @override
+  _TransactionsViewState createState() => _TransactionsViewState();
+}
+
+class _TransactionsViewState extends State<TransactionsView> {
+  int nbTransactions = 30;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: widget.animationController,
+      builder: (BuildContext context, Widget child) {
+        return FutureBuilder(
+            future: httpService.getDnaAll(),
+            builder: (BuildContext context, AsyncSnapshot<DnaAll> snapshot) {
+              if (snapshot.hasData) {
+                dnaAll = snapshot.data;
+                if (dnaAll == null || dnaAll.dnaIdentityResponse == null) {
+                  return Text("");
+                } else {
+                  return FadeTransition(
+                    opacity: widget.animation,
+                    child: new Transform(
+                      transform: new Matrix4.translationValues(
+                          0.0, 30 * (1.0 - widget.animation.value), 0.0),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 24, right: 24, top: 16, bottom: 18),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: MyIdenaAppTheme.white,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(8.0),
+                                bottomLeft: Radius.circular(8.0),
+                                bottomRight: Radius.circular(8.0),
+                                topRight: Radius.circular(68.0)),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                  color: MyIdenaAppTheme.grey.withOpacity(0.2),
+                                  offset: Offset(1.1, 1.1),
+                                  blurRadius: 10.0),
+                            ],
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 16, left: 16, right: 16),
+                                child: Row(
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(
+                                            left: 8, right: 8, top: 4),
+                                        child: Column(
+                                          children: <Widget>[
+                                            FutureBuilder(
+                                              future:
+                                                  httpService.getTransactions(
+                                                      dnaAll.dnaIdentityResponse
+                                                          .result.address,
+                                                      nbTransactions),
+                                              builder: (context, snapshot) {
+                                                if (!snapshot.hasData)
+                                                  return Container();
+                                                BcnTransactionsResponse
+                                                    bcnTransactionsResponse =
+                                                    snapshot.data;
+                                                List<Transaction> transactions =
+                                                    bcnTransactionsResponse
+                                                        .result.transactions;
+                                                return new Container(
+                                                  height: 400,
+                                                  child: ListView.builder(
+                                                      physics:
+                                                          BouncingScrollPhysics(),
+                                                      itemCount:
+                                                          transactions.length,
+                                                      itemBuilder:
+                                                          (BuildContext context,
+                                                              int index) {
+                                                        Transaction
+                                                            transaction =
+                                                            transactions[index];
+                                                        return getTransactionDisplay(
+                                                            transaction);
+                                                      }),
+                                                );
+                                              },
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            });
+      },
+    );
+  }
+
+  Widget getImageTo(Transaction transaction) {
+    if (transaction.from != null &&
+        transaction.from != dnaAll.dnaIdentityResponse.result.address) {
+      return Image.network(
+        'https://robohash.org/${transaction.from}',
+        width: 30,
+        height: 30,
+      );
+    } else {
+      return SizedBox(width: 30, height: 0);
+    }
+  }
+
+  Widget getFees(Transaction transaction) {
+    if (double.parse(transaction.usedFee) > 0) {
+      return Text(
+        transaction.usedFee == null
+            ? ""
+            : AppLocalizations.of(context).translate("Fees: ") +
+                transaction.usedFee +
+                " iDNA",
+        style: TextStyle(
+          fontFamily: MyIdenaAppTheme.fontName,
+          fontWeight: FontWeight.w600,
+          fontSize: 12,
+          color: MyIdenaAppTheme.darkText,
+        ),
+      );
+    } else {
+      return SizedBox(width: 0, height: 0);
+    }
+  }
+
+  Widget getAmount(Transaction transaction) {
+    if (double.parse(transaction.amount) > 0) {
+      if (transaction.from != null &&
+          transaction.from != dnaAll.dnaIdentityResponse.result.address) {
+        return Text(
+          transaction.amount == null
+              ? ""
+              : AppLocalizations.of(context).translate("Montant: ") +
+                  "+" +
+                  transaction.amount +
+                  " iDNA",
+          style: TextStyle(
+            fontFamily: MyIdenaAppTheme.fontName,
+            fontWeight: FontWeight.w600,
+            fontSize: 12,
+            color: MyIdenaAppTheme.darkText,
+          ),
+        );
+      } else {
+        return Text(
+          transaction.amount == null
+              ? ""
+              : AppLocalizations.of(context).translate("Montant: ") +
+                  "-" +
+                  transaction.amount +
+                  " iDNA",
+          style: TextStyle(
+            fontFamily: MyIdenaAppTheme.fontName,
+            fontWeight: FontWeight.w600,
+            fontSize: 12,
+            color: MyIdenaAppTheme.darkText,
+          ),
+        );
+      }
+    } else {
+      return SizedBox(width: 0, height: 0);
+    }
+  }
+
+  Widget getDirection(Transaction transaction) {
+    if (transaction.from != null &&
+        transaction.from != dnaAll.dnaIdentityResponse.result.address) {
+      return new Icon(Icons.vertical_align_bottom,
+          color: Colors.blue[600], size: 22.0);
+    } else {
+      return new Icon(Icons.vertical_align_top,
+          color: Colors.red[600], size: 22.0);
+    }
+  }
+
+  Widget getFrom(Transaction transaction) {
+    if (transaction.from != null &&
+        transaction.from != dnaAll.dnaIdentityResponse.result.address) {
+      return Text(
+        AppLocalizations.of(context).translate("From: ") +
+            transaction.from.substring(0, 15) +
+            "...",
+        style: TextStyle(
+          fontFamily: MyIdenaAppTheme.fontName,
+          fontWeight: FontWeight.w600,
+          fontSize: 12,
+          color: MyIdenaAppTheme.darkText,
+        ),
+      );
+    } else {
+      return SizedBox(width: 0, height: 0);
+    }
+  }
+
+  Widget getTransactionDisplay(Transaction transaction) {
+    var typeAccepted = [
+      "send",
+      "activation",
+      "invite",
+      "killInvitee",
+      "kill",
+      "submitFlip",
+      "online",
+    ];
+    if (typeAccepted.contains(transaction.type)) {
+      return Container(
+          child: new Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        getDirection(transaction),
+                        getTransactionType(transaction),
+                      ],
+                    ),
+                  ],
+                ),
+                Text(
+                  new DateFormat.yMd(Intl.defaultLocale)
+                      .add_jm()
+                      .format(DateTime.fromMillisecondsSinceEpoch(
+                          transaction.timestamp * 1000))
+                      .toString(),
+                  style: TextStyle(
+                    fontFamily: MyIdenaAppTheme.fontName,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 12,
+                    color: MyIdenaAppTheme.darkText,
+                  ),
+                ),
+              ]),
+          new Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  width: 30,
+                  height: 0,
+                ),
+                getImageTo(transaction),
+                new Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      getFrom(transaction),
+                      getAmount(transaction),
+                      getFees(transaction),
+                    ]),
+              ]),
+          Column(
+            children: [
+              new Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new Icon(Icons.check_circle_outline,
+                        color: Colors.green, size: 15.0),
+                    SizedBox(
+                      width: 5,
+                      height: 10,
+                    ),
+                    Text(
+                      transaction.hash == null
+                          ? ""
+                          : AppLocalizations.of(context)
+                                  .translate("Blockchain transaction ID: ") +
+                              transaction.hash.substring(0, 15) +
+                              "...",
+                      style: TextStyle(
+                        fontFamily: MyIdenaAppTheme.fontName,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 12,
+                        color: MyIdenaAppTheme.darkText,
+                      ),
+                    ),
+                  ]),
+            ],
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 0, right: 0, top: 8, bottom: 8),
+            child: Container(
+              height: 2,
+              decoration: BoxDecoration(
+                color: MyIdenaAppTheme.background,
+                borderRadius: BorderRadius.all(Radius.circular(4.0)),
+              ),
+            ),
+          ),
+        ],
+      ));
+    } else {
+      return Container(width: 0, height: 0);
+    }
+  }
+
+  Widget getTransactionType(Transaction transaction) {
+    String type = transaction.type;
+    String payload = transaction.payload;
+    String text;
+    if (type == 'send') {
+      if (transaction.from != null &&
+          transaction.from != dnaAll.dnaIdentityResponse.result.address) {
+        text = "Received";
+      } else {
+        text = "Sent";
+      }
+    } else {
+      if (type == 'activation') {
+        text = "Invitation activated";
+      } else {
+        if (type == 'invite') {
+          text = "Invitation issued";
+        } else {
+          if (type == 'killInvitee') {
+            text = "Invitation terminated";
+          } else {
+            if (type == 'kill') {
+              text = "Identity terminated";
+            } else {
+              if (type == 'submitFlip') {
+                text = "Flip submitted";
+              } else {
+                if (type == 'online') {
+                  if (payload == '0x') {
+                    text = "Mining status Off";
+                  } else {
+                    text = "Mining status On";
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    if (text == null) {
+      text = "Transaction type unknown";
+    }
+    return Text(
+      AppLocalizations.of(context).translate(text),
+      style: TextStyle(
+        fontFamily: MyIdenaAppTheme.fontName,
+        fontWeight: FontWeight.w600,
+        fontSize: 12,
+        color: MyIdenaAppTheme.darkText,
+      ),
+    );
+  }
+}
