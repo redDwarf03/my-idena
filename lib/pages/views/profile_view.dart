e153ee735062ddf2da4c1c960ca13c41d61718bd
redDwarf03 <a_sacris_sylvain@yahoo.fr>
2020-08-26 20:02:05 +0200

    wrong import

--- lib/pages/views/profile_view.dart
+++ lib/pages/views/profile_view.dart
@@ -1,4 +1,3 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:my_idena/backoffice/bean/dna_all.dart';
