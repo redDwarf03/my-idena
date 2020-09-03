f67b011664a273a0b1fa7423a091b309cb5bca3f
SSe03 <sseramy@nse-groupe.com>
2020-09-03 18:21:20 +0200

    pb with deep link

--- lib/main.dart
+++ lib/main.dart
@@ -41,14 +41,171 @@ class MyApp extends StatefulWidget {
 enum UniLinksType { string, uri }
 
 class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  StreamSubscription _sub;
+  UniLinksType _type = UniLinksType.string;
+  String _latestLink = 'Unknown';
+  Uri _latestUri;
+
   @override
   initState() {
     super.initState();
-    // initPlatformState();
+    initPlatformState();
+  }
+
+  @override
+  dispose() {
+    if (_sub != null) _sub.cancel();
+    super.dispose();
+  }
+
+  initPlatformState() async {
+    if (_type == UniLinksType.string) {
+      await initPlatformStateForStringUniLinks();
+    } else {
+      await initPlatformStateForUriUniLinks();
+    }
+  }
+
+  initPlatformStateForStringUniLinks() async {
+    // Attach a listener to the links stream
+    _sub = getLinksStream().listen((String link) {
+      if (!mounted) return;
+      setState(() {
+        _latestLink = link ?? 'Unknown';
+        _latestUri = null;
+        try {
+          if (link != null) _latestUri = Uri.parse(link);
+        } on FormatException {}
+      });
+    }, onError: (err) {
+      if (!mounted) return;
+      setState(() {
+        _latestLink = 'Failed to get latest link: $err.';
+        _latestUri = null;
+      });
+    });
+
+    // Attach a second listener to the stream
+    getLinksStream().listen((String link) {
+      print('got link: $link');
+    }, onError: (err) {
+      print('got err: $err');
+    });
+
+    // Get the latest link
+    String initialLink;
+    Uri initialUri;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      initialLink = await getInitialLink();
+      print('initial link: $initialLink');
+      if (initialLink != null) initialUri = Uri.parse(initialLink);
+    } on PlatformException {
+      initialLink = 'Failed to get initial link.';
+      initialUri = null;
+    } on FormatException {
+      initialLink = 'Failed to parse the initial link as Uri.';
+      initialUri = null;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _latestLink = initialLink;
+      _latestUri = initialUri;
+    });
+  }
+
+  /// An implementation using the [Uri] convenience helpers
+  initPlatformStateForUriUniLinks() async {
+    // Attach a listener to the Uri links stream
+    _sub = getUriLinksStream().listen((Uri uri) {
+      if (!mounted) return;
+      setState(() {
+        _latestUri = uri;
+        _latestLink = uri?.toString() ?? 'Unknown';
+      });
+    }, onError: (err) {
+      if (!mounted) return;
+      setState(() {
+        _latestUri = null;
+        _latestLink = 'Failed to get latest link: $err.';
+      });
+    });
+
+    // Attach a second listener to the stream
+    getUriLinksStream().listen((Uri uri) {
+      print('got uri: ${uri?.path} ${uri?.queryParametersAll}');
+    }, onError: (err) {
+      print('got err: $err');
+    });
+
+    // Get the latest Uri
+    Uri initialUri;
+    String initialLink;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      initialUri = await getInitialUri();
+      print('initial uri: ${initialUri?.path}'
+          ' ${initialUri?.queryParametersAll}');
+      initialLink = initialUri?.toString();
+    } on PlatformException {
+      initialUri = null;
+      initialLink = 'Failed to get initial uri.';
+    } on FormatException {
+      initialUri = null;
+      initialLink = 'Bad parse the initial link as Uri.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _latestUri = initialUri;
+      _latestLink = initialLink;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    final list = _latestUri?.queryParametersAll?.entries?.toList();
+    if (list != null) {
+      deepLinkParam = new DeepLinkParam();
+      for (var i = 0; i < list.length; i++) {
+        switch (list[i].key) {
+          case "nonce_endpoint":
+            {
+              deepLinkParam.nonceEndpoint = list[i].value[0];
+            }
+            break;
+          case "token":
+            {
+              deepLinkParam.token = list[i].value[0];
+            }
+            break;
+          case "callback_url":
+            {
+              deepLinkParam.callbackUrl = list[i].value[0];
+            }
+            break;
+          case "authentication_endpoint":
+            {
+              deepLinkParam.authenticationEndpoint = list[i].value[0];
+            }
+            break;
+        }
+      }
+      logger.i("nonce_endpoint: " + deepLinkParam.nonceEndpoint);
+      logger.i("token: " + deepLinkParam.token);
+      logger.i("callback_url: " + deepLinkParam.callbackUrl);
+      logger.i(
+          "authentication_endpoint: " + deepLinkParam.authenticationEndpoint);
+    }
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
       statusBarIconBrightness: Brightness.dark,
@@ -82,48 +239,11 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
           textTheme: MyIdenaAppTheme.textTheme,
           platform: TargetPlatform.iOS,
         ),
-        home: StreamBuilder(
-          stream: getLinksStream(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              var uri = Uri.parse(snapshot.data);
-              var list = uri.queryParametersAll.entries.toList();
-              deepLinkParam = new DeepLinkParam();
-              for (var i = 0; i < list.length; i++) {
-                switch (list[i].key) {
-                  case "nonce_endpoint":
-                    {
-                      deepLinkParam.nonceEndpoint = list[i].value[0];
-                    }
-                    break;
-                  case "token":
-                    {
-                      deepLinkParam.token = list[i].value[0];
-                    }
-                    break;
-                  case "callback_url":
-                    {
-                      deepLinkParam.callbackUrl = list[i].value[0];
-                    }
-                    break;
-                  case "authentication_endpoint":
-                    {
-                      deepLinkParam.authenticationEndpoint = list[i].value[0];
-                    }
-                    break;
-                }
-              }
-              logger.i("nonce_endpoint: " + deepLinkParam.nonceEndpoint);
-              logger.i("token: " + deepLinkParam.token);
-              logger.i("callback_url: " + deepLinkParam.callbackUrl);
-              logger.i("authentication_endpoint: " +
-                  deepLinkParam.authenticationEndpoint);
-              return DeepLinkScreen(deepLinkParam: deepLinkParam,);
-            } else {
-              return RouteScreen();
-            }
-          },
-        ),
+        home: list != null && list.length > 0
+            ? new DeepLinkScreen(
+                deepLinkParam: deepLinkParam,
+              )
+            : new RouteScreen(),
       ),
     );
   }
