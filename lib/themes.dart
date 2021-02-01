cb7a73a2daaca724ea783195442dee42626c3d12
redDwarf03 <rddwarf03@gmail.com>
2021-02-01 13:34:02 +0100

    change status bar color

--- lib/themes.dart
+++ lib/themes.dart
@@ -138,7 +138,7 @@ class IdenaTheme extends BaseTheme {
 
   Brightness brightness = Brightness.dark;
   SystemUiOverlayStyle statusBar =
-      SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.red);
+      SystemUiOverlayStyle.dark.copyWith(statusBarColor: teal);
 
   BoxShadow boxShadow = BoxShadow(color: Colors.transparent);
   BoxShadow boxShadowButton = BoxShadow(color: Colors.transparent);
