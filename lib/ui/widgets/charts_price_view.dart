c403c3dc4dd7c6cc36991c5ef4fd6d0742d09aab
redDwarf03 <rddwarf03@gmail.com>
2021-02-10 18:17:42 +0100

    fix bugs

--- lib/ui/widgets/charts_price_view.dart
+++ lib/ui/widgets/charts_price_view.dart
@@ -9,113 +9,106 @@ import 'package:my_idena/service_locator.dart';
 
 class ChartsPriceView extends StatefulWidget {
   final AvailableCurrency localCurrency;
+  final int nbDays;
 
-  const ChartsPriceView({Key key, this.localCurrency}) : super(key: key);
+  const ChartsPriceView({Key key, this.localCurrency, this.nbDays})
+      : super(key: key);
 
   @override
   _ChartsPriceViewState createState() => _ChartsPriceViewState();
 }
 
 class _ChartsPriceViewState extends State<ChartsPriceView> {
-  List<charts.Series<Price, DateTime>> _seriesLineData;
+  List<charts.Series<Price, DateTime>> _seriesLineData = [];
+  List<Price> chartDataList = [];
+  Widget lineChart = Center(child: CircularProgressIndicator());
+  int nbDaysSave;
 
-  @override
-  initState() {
+  void initState() {
     super.initState();
-    _seriesLineData = List<charts.Series<Price, DateTime>>();
+    nbDaysSave = 0;
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<CoinsPriceResponse>(
-        future: sl
-            .get<CoinsService>()
-            .getCoinsChart(widget.localCurrency.getIso4217Code(), 7),
-        builder:
-            (BuildContext context, AsyncSnapshot<CoinsPriceResponse> snapshot) {
-          if (snapshot.hasData == false) {
-            return Center(child: CircularProgressIndicator());
-          } else {
-            CoinsPriceResponse coinsPriceResponse = snapshot.data;
-            List<Price> linePricesData = new List();
-            double min = 10;
-            double max = 0;
-            for (int i = 0; i < coinsPriceResponse.prices.length; i++) {
-              if (min > coinsPriceResponse.prices[i][1].toDouble()) {
-                min = coinsPriceResponse.prices[i][1].toDouble();
-              }
-              if (max < coinsPriceResponse.prices[i][1].toDouble()) {
-                max = coinsPriceResponse.prices[i][1].toDouble();
-              }
-              Price price = new Price(
-                  DateTime.fromMillisecondsSinceEpoch(
-                      coinsPriceResponse.prices[i][0].toInt()),
-                  coinsPriceResponse.prices[i][1].toDouble());
-              linePricesData.add(price);
-            }
+    if (nbDaysSave != widget.nbDays) {
+      chartDataList = [];
+      _seriesLineData = [];
+      Config.reinit();
+      lineChart = Center(child: CircularProgressIndicator());
+    }
+    if (!Config.isLoaded())
+      Config.loadChartDataList(
+              widget.localCurrency.getIso4217Code(), widget.nbDays)
+          .then((value) => setState(() {
+                chartDataList = Config.getChartDataList(
+                    widget.localCurrency.getIso4217Code(), widget.nbDays);
 
-            _seriesLineData.add(
-              charts.Series<Price, DateTime>(
-                colorFn: (__, _) =>
-                    charts.ColorUtil.fromDartColor(Color(0xff990099)),
-                id: 'Price',
-                data: linePricesData,
-                domainFn: (Price price, _) => price.dateVal,
-                measureFn: (Price price, _) => price.priceVal,
-              ),
-            );
+                _seriesLineData.add(
+                  charts.Series<Price, DateTime>(
+                    colorFn: (__, _) =>
+                        charts.ColorUtil.fromDartColor(Color(0xff990099)),
+                    id: 'Price',
+                    data: chartDataList,
+                    domainFn: (Price price, _) => price.dateVal,
+                    measureFn: (Price price, _) => price.priceVal,
+                  ),
+                );
 
-            return Container(
-                height: 200,
-                width: double.infinity,
-                margin: EdgeInsets.only(
-                  left: MediaQuery.of(context).size.width * 0.0750,
-                  right: MediaQuery.of(context).size.width * 0.0750,
-                ),
-                child: Center(
-                    child: Column(children: <Widget>[
-                  Expanded(
-                      child: charts.TimeSeriesChart(_seriesLineData,
-                          primaryMeasureAxis: new charts.NumericAxisSpec(
-                            tickProviderSpec:
-                                new charts.StaticNumericTickProviderSpec(
-                              <charts.TickSpec<num>>[
-                                charts.TickSpec<num>(min),
-                                charts.TickSpec<num>(min + ((max - min) / 4)),
-                                charts.TickSpec<num>(
-                                    min + ((max - min) / 4) * 2),
-                                charts.TickSpec<num>(
-                                    min + ((max - min) / 4) * 3),
-                                charts.TickSpec<num>(max),
-                              ],
-                            ),
+                nbDaysSave = widget.nbDays;
+
+                lineChart = charts.TimeSeriesChart(_seriesLineData,
+                    primaryMeasureAxis: new charts.NumericAxisSpec(
+                      tickProviderSpec:
+                          new charts.StaticNumericTickProviderSpec(
+                        <charts.TickSpec<num>>[
+                          charts.TickSpec<num>(Config.min),
+                          charts.TickSpec<num>(
+                              Config.min + ((Config.max - Config.min) / 4)),
+                          charts.TickSpec<num>(
+                              Config.min + ((Config.max - Config.min) / 4) * 2),
+                          charts.TickSpec<num>(
+                              Config.min + ((Config.max - Config.min) / 4) * 3),
+                          charts.TickSpec<num>(Config.max),
+                        ],
+                      ),
+                    ),
+                    defaultRenderer: new charts.LineRendererConfig(
+                        includeArea: true, stacked: true),
+                    animate: false,
+                    dateTimeFactory: const charts.LocalDateTimeFactory(),
+                    behaviors: [
+                      new charts.ChartTitle(
+                          AppLocalization.of(context).chartDate,
+                          titleStyleSpec: charts_common.TextStyleSpec(
+                            fontSize: 12,
                           ),
-                          defaultRenderer: new charts.LineRendererConfig(
-                              includeArea: true, stacked: true),
-                          animate: false,
-                          dateTimeFactory: const charts.LocalDateTimeFactory(),
-                          behaviors: [
-                        new charts.ChartTitle(AppLocalization.of(context).chartDate,
-                            titleStyleSpec: charts_common.TextStyleSpec(
-                              fontSize: 12,
-                            ),
-                            behaviorPosition: charts.BehaviorPosition.bottom,
-                            titleOutsideJustification:
-                                charts.OutsideJustification.middleDrawArea),
-                        new charts.ChartTitle(
-                            AppLocalization.of(context).chartPrice + " (" +
-                                widget.localCurrency.getIso4217Code() +
-                                ")",
-                            titleStyleSpec: charts_common.TextStyleSpec(
-                              fontSize: 12,
-                            ),
-                            behaviorPosition: charts.BehaviorPosition.start,
-                            titleOutsideJustification:
-                                charts.OutsideJustification.middleDrawArea),
-                      ]))
-                ])));
-          }
-        });
+                          behaviorPosition: charts.BehaviorPosition.bottom,
+                          titleOutsideJustification:
+                              charts.OutsideJustification.middleDrawArea),
+                      new charts.ChartTitle(
+                          AppLocalization.of(context).chartPrice +
+                              " (" +
+                              widget.localCurrency.getIso4217Code() +
+                              ")",
+                          titleStyleSpec: charts_common.TextStyleSpec(
+                            fontSize: 12,
+                          ),
+                          behaviorPosition: charts.BehaviorPosition.start,
+                          titleOutsideJustification:
+                              charts.OutsideJustification.middleDrawArea),
+                    ]);
+              }));
+
+    return Container(
+        height: 200,
+        width: double.infinity,
+        margin: EdgeInsets.only(
+          left: MediaQuery.of(context).size.width * 0.0750,
+          right: MediaQuery.of(context).size.width * 0.0750,
+        ),
+        child: Center(
+            child: Column(children: <Widget>[Expanded(child: lineChart)])));
   }
 }
 
@@ -124,4 +117,48 @@ class Price {
   double priceVal;
 
   Price(this.dateVal, this.priceVal);
+}
+
+class Config {
+  static List<Price> _chartDataList = [];
+  static bool _loaded = false;
+  static double min = 10;
+  static double max = 0;
+
+  static isLoaded() {
+    return _loaded;
+  }
+
+  static reinit() {
+    _loaded = false;
+    _chartDataList = [];
+    min = 10;
+    max = 0;
+  }
+
+  static loadChartDataList(String iso4217Code, int nbDays) async {
+    CoinsPriceResponse coinsPriceResponse =
+        await sl.get<CoinsService>().getCoinsChart(iso4217Code, nbDays);
+
+    for (int i = 0; i < coinsPriceResponse.prices.length; i++) {
+      if (min > coinsPriceResponse.prices[i][1].toDouble()) {
+        min = coinsPriceResponse.prices[i][1].toDouble();
+      }
+      if (max < coinsPriceResponse.prices[i][1].toDouble()) {
+        max = coinsPriceResponse.prices[i][1].toDouble();
+      }
+      Price price = new Price(
+          DateTime.fromMillisecondsSinceEpoch(
+              coinsPriceResponse.prices[i][0].toInt()),
+          coinsPriceResponse.prices[i][1].toDouble());
+      _chartDataList.add(price);
+    }
+
+    _loaded = true;
+  }
+
+  static getChartDataList(String iso4217Code, int nbDays) {
+    if (!_loaded) loadChartDataList(iso4217Code, nbDays);
+    return _chartDataList;
+  }
 }
