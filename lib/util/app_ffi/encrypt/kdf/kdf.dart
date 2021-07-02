650e44d32545c49294108df9116bedde9aef48d9
redDwarf03 <rddwarf03@gmail.com>
2021-07-02 16:45:41 +0200

    lib_dart

--- lib/util/app_ffi/encrypt/kdf/kdf.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:typed_data';
-import 'package:my_idena/util/app_ffi/encrypt/model/keyiv.dart';
-
-/// KDF (Key derivator function) base class
-abstract class KDF {
-  /// Derive a KeyIV with given password and optional salt
-  KeyIV deriveKey(String password, {Uint8List salt});
-}
--- lib/util/enums/answer_type.dart
+++
@@ -1,8 +0,0 @@
-library answer_type;
-
-const int NONE = 0;
-const int LEFT =  1;
-const int RIGHT = 2;
-const int INAPPROPRIATE = 3;
-
-
